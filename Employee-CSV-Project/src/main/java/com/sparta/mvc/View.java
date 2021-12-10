132dc33d5e8abae4d20ea256a22129b81236bdac
Ishmael <ishariff@spartaglobal.com>
2021-12-10 11:19:16 +0000

    fix

--- Employee-CSV-Project/src/main/java/com/sparta/mvc/View.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.mvc;
-
-public class View {
-    public void menu(){
-        System.out.println("Please pick an option from below");
-        System.out.println("1. Populate the database from a CSV");
-    }
-}
